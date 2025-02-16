afc00a65fdb2ecb40869c18964acd2049e65d04a
Mohamed <mohamed.kholy2011@gmail.com>
2025-02-16 21:53:05 +0200

    Update

--- Laravel/app/Providers/AppServiceProvider.php
+++ Laravel/app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Laravel\Telescope\Telescope;
 use Laravel\Telescope\TelescopeApplicationServiceProvider;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +28,10 @@ class AppServiceProvider extends ServiceProvider
         Telescope::filter(function ($entry) {
             return app()->environment('local') || Auth::check();
         });
+
+        // Change date for testing
+        // if ($this->app->environment('local', 'testing')) {
+        //     Carbon::setTestNow(Carbon::now()->addMonth());
+        // }
     }
 }
