57bfdcebcd3617b73a8109b6a859aee6ecad6910
Mohamed <mohamed.kholy2011@gmail.com>
2025-02-16 22:26:06 +0200

    Update main.ts

--- Angular/src/main.ts
+++ Angular/src/main.ts
@@ -4,6 +4,24 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 
+// // Backup the original Date constructor
+// const RealDate = Date;
+
+// class MockDate extends RealDate {
+//   constructor(...args: any[]) {
+//     if (args.length === 0) {
+//       const tempDate = new RealDate();
+//       tempDate.setMonth(tempDate.getMonth() + 1); // Add one month to current date
+//       super(tempDate.getTime());
+//     } else {
+//       super(...args as [any]);
+//     }
+//   }
+// }
+
+// // Override global Date (FOR TESTING ONLY)
+// globalThis.Date = MockDate as unknown as DateConstructor;
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
