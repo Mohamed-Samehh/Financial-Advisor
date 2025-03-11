f1c70db0b17799e1c37733c363f0c7e2a4ae75f9
Mohamed <mohamed.kholy2011@gmail.com>
2025-03-11 12:38:34 +0200

    Update login_screen.dart

--- Flutter/lib/screens/login_screen.dart
+++ Flutter/lib/screens/login_screen.dart
@@ -248,7 +248,7 @@ class LoginScreenState extends State<LoginScreen> {
                                       if (!RegExp(
                                         r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
                                       ).hasMatch(value)) {
-                                        return 'Please enter a valid email address.';
+                                        return 'Invalid email address';
                                       }
                                       return null;
                                     },
