35a8c60797ec02bc342a3ac487b6a0d9176e4928
Mohamed <mohamed.kholy2011@gmail.com>
2025-04-06 22:22:26 +0200

    Update

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -5,8 +5,6 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { ExpensesComponent } from './expenses/expenses.component';
-import { BudgetComponent } from './budget/budget.component';
-import { GoalsComponent } from './goals/goals.component';
 import { AnalyzeExpensesComponent } from './analyze-expenses/analyze-expenses.component';
 import { InvestComponent } from './invest/invest.component';
 import { ChatbotComponent } from './chatbot/chatbot.component';
@@ -17,8 +15,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'budget', component: BudgetComponent, canActivate: [AuthGuard] },
-  { path: 'goal', component: GoalsComponent, canActivate: [AuthGuard] },
   { path: 'categories', component: CategoriesComponent, canActivate: [AuthGuard] },
   { path: 'expenses', component: ExpensesComponent, canActivate: [AuthGuard] },
   { path: 'analyze', component: AnalyzeExpensesComponent, canActivate: [AuthGuard] },
